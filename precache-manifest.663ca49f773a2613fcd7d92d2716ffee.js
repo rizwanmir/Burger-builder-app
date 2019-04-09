bfb526199a1c746b98299bf05d2f0f30d0a9b114
Rizwan Mir <rizwan_mir86@hotmail.com>
2019-04-09 13:40:58 +0200

    Updates

---
+++ precache-manifest.663ca49f773a2613fcd7d92d2716ffee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b16f6bb2b918b987d6",
+    "url": "/Burger-builder-app/static/css/main.b497037f.chunk.css"
+  },
+  {
+    "revision": "94b16f6bb2b918b987d6",
+    "url": "/Burger-builder-app/static/js/main.94b16f6b.chunk.js"
+  },
+  {
+    "revision": "54252f1a25fc928b3cc2",
+    "url": "/Burger-builder-app/static/js/1.54252f1a.chunk.js"
+  },
+  {
+    "revision": "781dc78b0773c8fe6829",
+    "url": "/Burger-builder-app/static/js/runtime~main.781dc78b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-builder-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "490a4e7413d238dd6e4f46679619670e",
+    "url": "/Burger-builder-app/index.html"
+  }
+];
